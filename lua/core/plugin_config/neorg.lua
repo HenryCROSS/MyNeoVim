0a0dcfa7790437ae5a59327a9490f5fb5890031d
henrycross <henry583050973@gmail.com>
2022-02-27 02:32:09 -0500

    change neorg

--- lua/core/plugin_config/neorg.lua
+++ lua/core/plugin_config/neorg.lua
@@ -4,6 +4,13 @@ M.load = function ()
     require('neorg').setup {
         load = {
             ["core.defaults"] = {},
+            -- ["core.gtd.base"] = {},
+            -- ["core.norg.completion"] = {},
+            ["core.norg.concealer"] = {},
+            ["core.norg.journal"] = {},
+            ["core.norg.qol.toc"] = {},
+            -- ["core.presenter"] = {},
+            ["core.keybinds"] = {},
             ["core.norg.dirman"] = {
                 config = {
                     workspaces = {
