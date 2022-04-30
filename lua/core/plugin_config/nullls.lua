78a92da9dd20a13b158321ace55d2d539e9d6fb8
HenryCROSS <henry583050973@gmail.com>
2022-04-30 19:07:00 -0400

    change null-ls config

--- lua/core/plugin_config/nullls.lua
+++ lua/core/plugin_config/nullls.lua
@@ -8,20 +8,20 @@ local sources = {
     null_settings.builtins.formatting.shellharden,
     null_settings.builtins.formatting.shfmt,
 
-    null_settings.builtins.code_actions.gitsigns,
+    -- null_settings.builtins.code_actions.gitsigns,
 
     null_settings.builtins.diagnostics.cppcheck,
 
-    null_settings.builtins.formatting.lua_format,
+    -- null_settings.builtins.formatting.lua_format,
     null_settings.builtins
         .formatting
         .clang_format
         .with {
-        args = {
-            "-assume-filename=<FILENAME>",
-            "-style={BasedOnStyle: Microsoft, IndentWidth: 4}",
+            args = {
+                "-assume-filename=<FILENAME>",
+                "-style={BasedOnStyle: Microsoft, IndentWidth: 4}",
+            },
         },
-    },
     null_settings.builtins
         .formatting
         .prettierd,
@@ -30,28 +30,27 @@ local sources = {
 
 null.name = "jose-elias-alvarez/null-ls"
 
-null.load =
-    function()
-        null_settings.setup(
-            {
-                sources = sources,
-            }
-        )
-        -- vim.api.nvim_del_keymap("n","<Space>f")
-        vim.api
-            .nvim_set_keymap(
+null.load = function()
+    null_settings.setup(
+        {
+            sources = sources,
+        }
+    )
+    -- vim.api.nvim_del_keymap("n","<Space>f")
+    vim.api
+        .nvim_set_keymap(
             "v",
             "<Leader>cf",
             ":lua vim.lsp.buf.formatting()<CR>",
             {}
         )
-        vim.api
-            .nvim_set_keymap(
+    vim.api
+        .nvim_set_keymap(
             "n",
             "<Leader>cf",
             ":lua vim.lsp.buf.formatting()<CR>",
-            {noremap = true}
+            { noremap = true }
         )
-    end
+end
 
 return null
