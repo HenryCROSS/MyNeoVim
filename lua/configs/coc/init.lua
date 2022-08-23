1151cc9266f470dae2d7659a31c12d9ec555afb5
HenryCROSS <henry583050973@gmail.com>
2022-08-23 14:05:12 -0400

    rollback coc ver.

--- lua/configs/coc/init.lua
+++ lua/configs/coc/init.lua
@@ -3,7 +3,9 @@ return {
 	{
 		type = PROPERTY.PLUGIN_CONFIG,
 		name = "neoclide/coc.nvim",
-		branch = "release",
+		-- branch = "release",
+        commit = "5fce9a5",
+        run = 'yarn install --frozen-lockfile',
 		config = function ()
 			require("configs.coc.config")
 		end
